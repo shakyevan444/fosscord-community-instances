e630c431ff861c85b387aed8a6306a3d3697de18
Stilic <lateman-jpeg@outlook.fr>
2021-09-04 11:16:26 +0200

    add others instances

--- generate.py
+++ generate.py
@@ -4,7 +4,10 @@ data = json.loads(open("instances.json", "r").read())
 with open("README.md", "w") as readme:
     rendered_data = ""
     for i in data:
-        rendered_data += "### [{0}]({1})\n![{0} image]({3})\n\n{2}\n".format(
-            i["name"], i["url"], i["description"], i["image"])
+        image_code = ""
+        if "image" in i and i["image"] != "":
+            image_code = "![{0} image]({1})\n\n".format(i["name"], i["image"])
+        rendered_data += "### [{0}]({1})\n{3}{2}\n".format(
+            i["name"], i["url"], i["description"], image_code)
     readme.write(open("README.template.md", "r").read().replace(
         "<% generated_content %>", rendered_data))
