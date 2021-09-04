8eae5e5743f9fff7ae666ff06823425051c2d71c
Stilic <lateman-jpeg@outlook.fr>
2021-09-04 10:46:19 +0200

    fix for real instances file

--- generate.py
+++ generate.py
@@ -5,6 +5,6 @@ with open("README.md", "w") as readme:
     rendered_data = ""
     for i in data:
         rendered_data += "### [{0}]({1})\n![{0} image]({3})\n\n{2}\n".format(
-            i[0], i[1], i[2], i[3])
+            i["name"], i["url"], i["description"], i["image"])
     readme.write(open("README.template.md", "r").read().replace(
         "<% generated_content %>", rendered_data))
