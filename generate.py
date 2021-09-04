d4071abd26f72e9731b7e1c3112680670f3156eb
Stilic <lateman-jpeg@outlook.fr>
2021-09-04 10:41:05 +0200

    change json array to list

--- generate.py
+++ generate.py
@@ -5,6 +5,6 @@ with open("README.md", "w") as readme:
     rendered_data = ""
     for i in data:
         rendered_data += "### [{0}]({1})\n![{0} image]({3})\n\n{2}\n".format(
-            data[i]["name"], data[i]["url"], data[i]["description"], data[i]["image"])
+            i[0], i[1], i[2], i[3])
     readme.write(open("README.template.md", "r").read().replace(
         "<% generated_content %>", rendered_data))
